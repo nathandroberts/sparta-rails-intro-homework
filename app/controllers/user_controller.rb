1a9ea92c660a55ad7eac1fea4d995184c329c517
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-21 22:10:53 +0000

    basic setup complete, views created

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,14 @@
 class UserController < ApplicationController
+  def index
+
+  end
+  def new
+    @id = params[:id].to_i
+  end
+  def show
+    @id = params[:id].to_i
+  end
+  def edit
+    @id = params[:id].to_i
+  end
 end
